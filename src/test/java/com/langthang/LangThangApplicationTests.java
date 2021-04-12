f9c4e7e94fcb3cef696073e2f8a7c7a0db1e05d8
trinhdvt <trinhvideo123@gmail.com>
2021-04-13 00:03:53 +0700

    Initial commit, create project structure and base dependencies

---
+++ src/test/java/com/langthang/LangThangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langthang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangThangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
