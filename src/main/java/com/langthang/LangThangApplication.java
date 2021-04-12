77d91629d6dcc1721952aaf4572f08de6a48d008
trinhdvt <trinhvideo123@gmail.com>
2021-04-13 00:03:53 +0700

    Initial commit, create project structure and base dependencies

---
+++ src/main/java/com/langthang/LangThangApplication.java
@@ -0,0 +1,13 @@
+package com.langthang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangThangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangThangApplication.class, args);
+    }
+
+}
