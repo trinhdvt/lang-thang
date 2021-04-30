99a1e43270140d770cf52099d942473b68a9f763
trinhdvt <trinhvideo123@gmail.com>
2021-04-30 22:25:54 +0700

    a Validator Class for ValidImage Annotation

---
+++ src/main/java/com/langthang/annotation/ImageValidator.java
@@ -0,0 +1,37 @@
+package com.langthang.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ImageValidator implements ConstraintValidator<ValidImage, MultipartFile> {
+
+    @Value("${application.image.support-type}")
+    private String[] supportedTypes;
+
+    @Override
+    public void initialize(ValidImage constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext context) {
+        String contentType = multipartFile.getContentType();
+        if (!isSupportedContentType(contentType)) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Not support type")
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isSupportedContentType(String contentType) {
+        for (String type : supportedTypes) {
+            if (type.equals(contentType))
+                return true;
+        }
+        return false;
+    }
+}
