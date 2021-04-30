aed8e85994f4bb60f8b51e3c9921c63185565e7a
trinhdvt <trinhvideo123@gmail.com>
2021-04-30 22:27:12 +0700

    add Controller handle File Upload

---
+++ src/main/java/com/langthang/controller/upload/FileUploadController.java
@@ -0,0 +1,42 @@
+package com.langthang.controller.upload;
+
+import com.langthang.annotation.ValidImage;
+import com.langthang.services.StorageServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/upload")
+@Validated
+public class FileUploadController {
+
+    private final StorageServices storageServices;
+
+    @Autowired
+    public FileUploadController(StorageServices storageServices) {
+        this.storageServices = storageServices;
+    }
+
+    /**
+     * Upload a file to AWS S3 Storage
+     *
+     * @param multipartFile file to upload
+     * @return URL that client can access via browsers
+     */
+    @PostMapping
+    public ResponseEntity<Object> uploadFile(
+            @RequestParam("image") @ValidImage MultipartFile multipartFile) {
+        String uploadURL = storageServices.uploadFile(multipartFile);
+
+        return new ResponseEntity<>(uploadURL, HttpStatus.OK);
+    }
+
+
+}
