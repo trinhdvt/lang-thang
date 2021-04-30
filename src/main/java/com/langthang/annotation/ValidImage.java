bdd62c46daadb737eb507b6d07821b69883aea46
trinhdvt <trinhvideo123@gmail.com>
2021-04-30 22:24:41 +0700

    add Annotation for validating a image when upload to server

---
+++ src/main/java/com/langthang/annotation/ValidImage.java
@@ -0,0 +1,19 @@
+package com.langthang.annotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Constraint(validatedBy = {ImageValidator.class})
+public @interface ValidImage {
+    String message() default "Invalid image file";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
