e24597f8885565636568ec616cb90313bdb54cc5
trinhdvt <trinhvideo123@gmail.com>
2021-05-01 23:02:50 +0700

    refactor code + remove unnecessary Repository annotation

--- src/main/java/com/langthang/repository/AccountRepository.java
+++ src/main/java/com/langthang/repository/AccountRepository.java
@@ -2,9 +2,7 @@ package com.langthang.repository;
 
 import com.langthang.model.entity.Account;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AccountRepository extends CrudRepository<Account, Integer> {
 
     Account findByEmail(String email);
