c660655e21a5ca5afea196a8d5f1cb7e23988802
trinhdvt <trinhvideo123@gmail.com>
2021-05-01 22:52:58 +0700

    update Role class + Role now implement Spring Security's GrantedAuthority to config Authorization

--- src/main/java/com/langthang/model/entity/Role.java
+++ src/main/java/com/langthang/model/entity/Role.java
@@ -1,6 +1,13 @@
 package com.langthang.model.entity;
 
 
-public enum Role {
-    ROLE_MEMBER, ROLE_ADMIN
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_MEMBER, ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
