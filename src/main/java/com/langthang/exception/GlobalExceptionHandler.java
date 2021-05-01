553514be2c80572da9c825a0a97bcf6e7105b6fc
trinhdvt <trinhvideo123@gmail.com>
2021-05-01 22:49:06 +0700

    update GlobalExceptionHandler + remove exception's stack trace and name when return to client + add basic handle for CustomException

--- src/main/java/com/langthang/exception/GlobalExceptionHandler.java
+++ src/main/java/com/langthang/exception/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.langthang.exception;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
-import java.util.Date;
+import java.io.IOException;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,23 +23,39 @@ public class GlobalExceptionHandler {
     @Value("${spring.servlet.multipart.max-file-size}")
     private String maxFileSize;
 
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+            @Override
+            public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+                Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+                errorAttributes.remove("exception");
+                errorAttributes.remove("trace");
+                return errorAttributes;
+            }
+        };
+    }
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<Object> handleMaxUploadSizeExceed() {
-        CustomException customException = new CustomException();
-        customException.setMessage("File size cannot exceed " + maxFileSize);
-        customException.setStatus(HttpStatus.BAD_REQUEST.value());
-        customException.setTimestamp(new Date());
+        CustomResponse customResponse = new CustomResponse();
+        customResponse.setMessage("File size cannot exceed " + maxFileSize);
+        customResponse.setStatus(HttpStatus.BAD_REQUEST.value());
 
-        return new ResponseEntity<>(customException, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintViolation() {
-        CustomException customException = new CustomException();
-        customException.setMessage("Not support file type");
-        customException.setStatus(HttpStatus.BAD_REQUEST.value());
-        customException.setTimestamp(new Date());
+        CustomResponse customResponse = new CustomResponse();
+        customResponse.setMessage("Not support file type");
+        customResponse.setStatus(HttpStatus.BAD_REQUEST.value());
 
-        return new ResponseEntity<>(customException, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(customResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public void handleCustomException(HttpServletResponse res, CustomException ex) throws IOException {
+        res.sendError(ex.getHttpStatus().value(), ex.getMessage());
     }
 }
