d75139822839ed36028b3ae570862ac984ab6e9e
trinhdvt <trinhvideo123@gmail.com>
2021-05-01 22:49:41 +0700

    add EqualAndHashCode to Embeddable Key

--- src/main/java/com/langthang/model/entity/BookmarkedPostKey.java
+++ src/main/java/com/langthang/model/entity/BookmarkedPostKey.java
@@ -1,6 +1,7 @@
 package com.langthang.model.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class BookmarkedPostKey implements Serializable {
     private int accountId;
     private int postId;
