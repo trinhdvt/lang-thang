fe9985cb9969fa445079f101b5930b0340fc4749
trinhdvt <trinhvideo123@gmail.com>
2021-05-04 20:31:04 +0700

    rename file

--- src/main/java/com/langthang/services/impl/AwsStorageServicesImpl.java
+++ src/main/java/com/langthang/services/impl/AwsStorageServicesImpl.java
@@ -1,11 +1,9 @@
 package com.langthang.services.impl;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.langthang.services.StorageServices;
+import com.langthang.services.IStorageServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class AwsStorageServices implements StorageServices {
+public class AwsStorageServicesImpl implements IStorageServices {
 
     @Value("${cloud.aws.bucket.name}")
     private String bucketName;
@@ -29,7 +27,7 @@ public class AwsStorageServices implements StorageServices {
     private final AmazonS3 s3Client;
 
     @Autowired
-    public AwsStorageServices(AmazonS3 s3Client) {
+    public AwsStorageServicesImpl(AmazonS3 s3Client) {
         this.s3Client = s3Client;
     }
 
