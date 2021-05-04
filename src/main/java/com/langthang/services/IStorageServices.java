fb6eb00277e5b1aec7a05f9169f2dd8ec040042f
trinhdvt <trinhvideo123@gmail.com>
2021-05-04 20:31:04 +0700

    rename file

--- src/main/java/com/langthang/services/IStorageServices.java
+++ src/main/java/com/langthang/services/IStorageServices.java
@@ -2,7 +2,7 @@ package com.langthang.services;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public interface StorageServices {
+public interface IStorageServices {
     String uploadFile(MultipartFile multipartFile);
 
     String deleteFile(String filename);
