aa7f2b5e0ba980abf45b608293d6da9bc19f8812
trinhdvt <trinhvideo123@gmail.com>
2021-05-04 20:17:11 +0700

    validator class for validate email

---
+++ src/main/java/com/langthang/annotation/validator/EmailValidator.java
@@ -0,0 +1,29 @@
+package com.langthang.annotation.validator;
+
+import com.langthang.annotation.ValidEmail;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$";
+
+    @Override
+    public void initialize(ValidEmail constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return isValidEmail(email);
+    }
+
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
