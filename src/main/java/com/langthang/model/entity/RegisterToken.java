8c51ab952cf141dcd2c84aef40ca0daa41547396
trinhdvt <trinhvideo123@gmail.com>
2021-05-04 20:27:17 +0700

    add RegisterToken Entity to store user's register token

---
+++ src/main/java/com/langthang/model/entity/RegisterToken.java
@@ -0,0 +1,49 @@
+package com.langthang.model.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+@Table(name = "register_token")
+@Getter
+public class RegisterToken {
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String token;
+
+    @OneToOne(targetEntity = Account.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "account_id")
+    private Account account;
+
+    @Column(name = "expire_date")
+    private Date expireDate;
+
+    public RegisterToken() {
+    }
+
+    public RegisterToken(String token, Account account) {
+        this.token = token;
+        this.account = account;
+        this.expireDate = calculateExpireDate(EXPIRATION);
+    }
+
+    public void updateToken(String token) {
+        this.token = token;
+        this.expireDate = calculateExpireDate(EXPIRATION);
+    }
+
+    private Date calculateExpireDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Timestamp(cal.getTime().getTime()));
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+}
