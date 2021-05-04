3573d9a01b85978b9d8cbd887da3fe92ef527b8f
trinhdvt <trinhvideo123@gmail.com>
2021-05-04 20:24:16 +0700

    add an event for sending user register token which will trigger when user register to system

---
+++ src/main/java/com/langthang/event/OnRegistrationEvent.java
@@ -0,0 +1,27 @@
+package com.langthang.event;
+
+import com.langthang.model.entity.Account;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class OnRegistrationEvent extends ApplicationEvent {
+
+    private final Account account;
+    private final String appUrl;
+    private final String token;
+
+    public OnRegistrationEvent(Account account, String appUrl) {
+        super(account);
+        this.appUrl = appUrl;
+        this.account = account;
+        this.token = null;
+    }
+
+    public OnRegistrationEvent(Account account, String appUrl, String token) {
+        super(account);
+        this.account = account;
+        this.appUrl = appUrl;
+        this.token = token;
+    }
+}
