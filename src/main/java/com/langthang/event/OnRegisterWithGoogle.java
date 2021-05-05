6b8a2d6bb0e8705b98a8caa6287245e8db4d40da
trinhdvt <trinhvideo123@gmail.com>
2021-05-05 21:08:01 +0700

    add event to sending email if user's Google Account is not registered with system

---
+++ src/main/java/com/langthang/event/OnRegisterWithGoogle.java
@@ -0,0 +1,18 @@
+package com.langthang.event;
+
+import com.langthang.model.entity.Account;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class OnRegisterWithGoogle extends ApplicationEvent {
+    private final Account account;
+    private final String rawPassword;
+
+    public OnRegisterWithGoogle(Account account, String rawPassword) {
+        super(account);
+        this.account = account;
+        this.rawPassword = rawPassword;
+    }
+
+}
