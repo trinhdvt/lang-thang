5ad152e272127f1e2798cc138615e10400b0618a
trinhdvt <trinhvideo123@gmail.com>
2021-05-11 14:24:31 +0700

    add interface PostServices, contain need method to handle business logic about Post

---
+++ src/main/java/com/langthang/services/IPostServices.java
@@ -0,0 +1,36 @@
+package com.langthang.services;
+
+import com.langthang.dto.PostRequestDTO;
+import com.langthang.dto.PostResponseDTO;
+import com.langthang.model.entity.Post;
+
+import java.util.List;
+
+public interface IPostServices {
+    Post addNewPostOrDraft(PostRequestDTO postRequestDTO, String authorEmail, boolean isDraft);
+
+    Post findPostById(int postId);
+
+    boolean isPostNotFound(int postId);
+
+    PostResponseDTO getPostDetailById(int postId);
+
+    PostResponseDTO getPostDetailBySlug(String slug);
+
+    PostResponseDTO getDraftById(int postId);
+
+    List<PostResponseDTO> getPreviewPost(int page, int size);
+
+    List<PostResponseDTO> getPreviewPost(int page, int size, String keyword);
+
+    List<PostResponseDTO> getPopularPostByProperty(String propertyName, int size);
+
+    boolean checkResourceOwner(int postId, String ownerEmail);
+
+    void deletePostById(int postId);
+
+    Post updatePostById(int postId, PostRequestDTO postRequestDTO);
+
+    Post updateAndPublicDraft(PostRequestDTO postRequestDTO, Integer postId);
+
+}
