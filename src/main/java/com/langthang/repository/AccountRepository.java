7e95a6b5ac2a86e45788f9d6eae3d975034423d6
trinhdvt <trinhvideo123@gmail.com>
2021-05-11 14:21:29 +0700

    add function to count how many people is following an account

--- src/main/java/com/langthang/repository/AccountRepository.java
+++ src/main/java/com/langthang/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.langthang.repository;
 
 import com.langthang.model.entity.Account;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
@@ -8,4 +9,7 @@ public interface AccountRepository extends CrudRepository<Account, Integer> {
     Account findByEmail(String email);
 
     Account findByEmailAndEnabled(String email, boolean enabled);
+
+    @Query("select count(a) from FollowingRelationship a where a.accountId=?1")
+    int countFollowing(int accountId);
 }
