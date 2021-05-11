d7e015a765e88cd44fba18d536205a21ff1c3139
trinhdvt <trinhvideo123@gmail.com>
2021-05-11 14:02:27 +0700

    fix WARN that spring has more than one TaskExecutor

--- src/main/java/com/langthang/config/AsynchronousSpringEventsConfig.java
+++ src/main/java/com/langthang/config/AsynchronousSpringEventsConfig.java
@@ -2,6 +2,7 @@ package com.langthang.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
@@ -10,7 +11,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Configuration
 @EnableAsync
 public class AsynchronousSpringEventsConfig {
+
     @Bean
+    @Primary
     public ApplicationEventMulticaster applicationEventMulticaster() {
         SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
         eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
