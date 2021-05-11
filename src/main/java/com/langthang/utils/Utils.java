410346f516c1965b8da8ed4d1524c71d03c293f1
trinhdvt <trinhvideo123@gmail.com>
2021-05-11 14:25:23 +0700

    add utils class

---
+++ src/main/java/com/langthang/utils/Utils.java
@@ -0,0 +1,19 @@
+package com.langthang.utils;
+
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+public class Utils {
+
+    static Pattern vietnameseNormPattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+
+    public static String createSlug(String input) {
+        String temp = Normalizer.normalize(input, Normalizer.Form.NFD);
+        return vietnameseNormPattern.matcher(temp)
+                .replaceAll("")
+                .replaceAll("đ", "d")
+                .replaceAll("(?!-)\\W", "-")
+                .replaceAll("^-*|-*$", "")
+                .replaceAll("-{2,}", "-");
+    }
+}
