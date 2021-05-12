8ba60246a3f685a37b5833a6b660081a43b93df9
trinhdvt <trinhvideo123@gmail.com>
2021-05-12 21:09:09 +0700

    add AccountRepository

--- src/main/java/com/langthang/repository/AccountRepository.java
+++ src/main/java/com/langthang/repository/AccountRepository.java
@@ -1,15 +1,27 @@
 package com.langthang.repository;
 
 import com.langthang.model.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<Account, Integer> {
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
     Account findByEmail(String email);
 
-    Account findByEmailAndEnabled(String email, boolean enabled);
+    Account findAccountByEmailAndEnabled(String email, boolean enabled);
 
-    @Query("select count(a) from FollowingRelationship a where a.accountId=?1")
+    Account findAccountByIdAndEnabled(int accountId, boolean enabled);
+
+    @Query("select count(a) from FollowingRelationship a where a.followingAccountId=?1")
     int countFollowing(int accountId);
+
+    @Query("select count(bp) " +
+            "from BookmarkedPost bp join Post p on bp.post.id = p.id " +
+            "where p.account.id = ?1")
+    int countBookmarkOnMyPost(int accountId);
+
+    @Query("select count(c.id) " +
+            "from Comment c join Post p on c.post.id = p.id " +
+            "where p.account.id = ?1")
+    int countCommentOnMyPost(int accountId);
 }
