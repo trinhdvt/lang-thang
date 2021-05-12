6337df1af0a743f935dc3c366ff4054a7db0507e
trinhdvt <trinhvideo123@gmail.com>
2021-05-12 21:09:20 +0700

    add CommentRepository

--- src/main/java/com/langthang/repository/CommentRepository.java
+++ src/main/java/com/langthang/repository/CommentRepository.java
@@ -1,8 +1,18 @@
 package com.langthang.repository;
 
 import com.langthang.model.entity.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
 
+    @Query(value = "select count(account_id) " +
+            "from comment_like " +
+            "where comment_id=?1", nativeQuery = true)
+    int countCommentLike(int commentId);
+
+    @Query("select count(c) " +
+            "from Comment c  " +
+            "where c.post.id=?1")
+    int countCommentInPost(int postId);
 }
