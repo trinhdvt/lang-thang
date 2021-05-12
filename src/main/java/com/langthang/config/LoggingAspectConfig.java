b5f8bfbd385f3aa632a6fef79cafa2f065e12c3a
trinhdvt <trinhvideo123@gmail.com>
2021-05-12 10:05:29 +0700

    add config to enable log for every method in application

---
+++ src/main/java/com/langthang/config/LoggingAspectConfig.java
@@ -0,0 +1,70 @@
+package com.langthang.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspectConfig {
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointCut() {
+
+    }
+
+    @Pointcut("within(com.langthang.controller..*)" +
+            "|| within(com.langthang.services..*)")
+    public void applicationPointCut() {
+
+    }
+
+    @AfterThrowing(pointcut = "applicationPointCut() && springBeanPointCut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("Exception in {}.{} with cause = {}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                e.getCause() != null ? e.getCause() : "NULL");
+    }
+
+    @Around("applicationPointCut() && springBeanPointCut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        ObjectMapper jackson = new ObjectMapper();
+        if (log.isDebugEnabled()) {
+            log.debug("Enter: {}.{}() with arguments[s] = {}",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
+
+        try {
+            Object result = joinPoint.proceed();
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}.{}() with result = {}",
+                        joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(),
+                        jackson.writeValueAsString(result));
+            }
+            return result;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            log.error("Illegal argument: {} in {}.{}()",
+                    Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName());
+            throw throwable;
+        }
+
+    }
+}
