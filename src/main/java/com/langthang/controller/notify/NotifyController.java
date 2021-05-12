ed62d90318920630e9bd812b3da34fa4ab9eec23
trinhdvt <trinhvideo123@gmail.com>
2021-05-12 10:05:50 +0700

    remove Model Mapper

--- src/main/java/com/langthang/controller/notify/NotifyController.java
+++ src/main/java/com/langthang/controller/notify/NotifyController.java
@@ -3,8 +3,6 @@ package com.langthang.controller.notify;
 import com.langthang.dto.NotifyDTO;
 import com.langthang.event.OnNewNotifyEvent;
 import com.langthang.model.entity.Notify;
-import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +18,8 @@ import java.util.Date;
 @RestController
 @RequestMapping("/notify")
 @Validated
-@Slf4j
 public class NotifyController {
 
-    @Autowired
-    private ModelMapper modelMapper;
 
     @Autowired
     private ApplicationEventPublisher eventPublisher;
@@ -32,12 +27,9 @@ public class NotifyController {
     @PostMapping
     public ResponseEntity<Object> addNotification(
             @RequestBody @Valid NotifyDTO notifyDTO) {
-        log.info("Receiving new notify: {}", notifyDTO);
 
         Notify notify = dtoToEntity(notifyDTO);
-        log.info("Converted to Entity and attempting to save: {}", notify);
 
-        log.info("Broadcasting message to all client");
         eventPublisher.publishEvent(new OnNewNotifyEvent(notifyDTO.getAccountId()));
 
         return ResponseEntity.ok(notifyDTO);
@@ -46,6 +38,6 @@ public class NotifyController {
     private Notify dtoToEntity(NotifyDTO notifyDTO) {
         notifyDTO.setNotifyDate(new Date());
 
-        return modelMapper.map(notifyDTO, Notify.class);
+        return null;
     }
 }
