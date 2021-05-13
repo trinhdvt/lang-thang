626560a925e565e785c725fc17a2475b12390cf2
trinhdvt <trinhvideo123@gmail.com>
2021-05-13 10:43:23 +0700

    add constraint on password when register, password min length is 6

--- src/main/java/com/langthang/dto/UserDTO.java
+++ src/main/java/com/langthang/dto/UserDTO.java
@@ -1,7 +1,6 @@
 package com.langthang.dto;
 
 
-import com.langthang.annotation.PasswordMatches;
 import com.langthang.annotation.ValidEmail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,11 +8,11 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@PasswordMatches
 public class UserDTO {
     @NotNull
     @NotEmpty
@@ -21,7 +20,12 @@ public class UserDTO {
 
     @NotNull
     @NotEmpty
+    @Size(min = 6, message = "Password must contain at least 6 character")
     private String password;
+
+    @NotEmpty
+    @NotNull
+    @Size(min = 6)
     private String matchedPassword;
 
     @NotNull
