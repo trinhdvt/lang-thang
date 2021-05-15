ce509041fd74b0a3628708296530921e7056775b
trinhdvt <trinhvideo123@gmail.com>
2021-05-16 00:28:16 +0700

    wrapper class to hold user's basic info when user request to change basic info

---
+++ src/main/java/com/langthang/dto/AccountInfoDTO.java
@@ -0,0 +1,19 @@
+package com.langthang.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class AccountInfoDTO {
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    private String avatarLink;
+    private String fbLink;
+    private String instagramLink;
+    private String about;
+    private String occupation;
+}
