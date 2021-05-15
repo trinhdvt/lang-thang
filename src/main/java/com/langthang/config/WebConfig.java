ae28d1450f6534d15be8c50b6bae1da8a03261a6
trinhdvt <trinhvideo123@gmail.com>
2021-05-15 22:11:46 +0700

    create a WebMvcConfigurer Bean instead of implement its interface which may prevent Spring from load default config for Web

--- src/main/java/com/langthang/config/WebConfig.java
+++ src/main/java/com/langthang/config/WebConfig.java
@@ -1,17 +1,21 @@
 package com.langthang.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
 
