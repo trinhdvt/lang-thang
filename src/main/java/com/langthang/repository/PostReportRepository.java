9eb7fc86def8bc359926c5347b602b0e057de275
trinhdvt <trinhvideo123@gmail.com>
2021-05-16 00:36:55 +0700

    add PostReportRepository

---
+++ src/main/java/com/langthang/repository/PostReportRepository.java
@@ -0,0 +1,8 @@
+package com.langthang.repository;
+
+import com.langthang.model.entity.PostReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostReportRepository extends JpaRepository<PostReport, Integer> {
+
+}
