20f2e0be37a0c9cb43e28ade0bd67bf41628e5f1
trinhdvt <trinhvideo123@gmail.com>
2021-05-15 22:10:09 +0700

    create a TaskExecutor Bean for execute method Async in Spring

--- src/main/java/com/langthang/config/AsynchronousSpringEventsConfig.java
+++ src/main/java/com/langthang/config/AsynchronousSpringEventsConfig.java
@@ -1,22 +1,37 @@
 package com.langthang.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsynchronousSpringEventsConfig {
 
+    @Bean("taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.initialize();
+
+        return executor;
+    }
+
     @Bean
-    @Primary
-    public ApplicationEventMulticaster applicationEventMulticaster() {
+    @DependsOn({"taskExecutor"})
+    public ApplicationEventMulticaster applicationEventMulticaster(@Qualifier("taskExecutor") @Autowired Executor executor) {
         SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        eventMulticaster.setTaskExecutor(executor);
         return eventMulticaster;
     }
 }
