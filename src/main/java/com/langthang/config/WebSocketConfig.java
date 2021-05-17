ea89f7ddface6df9706a1fa41b4e9d373757cb67
trinhdvt <trinhvideo123@gmail.com>
2021-05-17 23:16:15 +0700

    configure JWT Token-based for authentication in web socket

--- src/main/java/com/langthang/config/WebSocketConfig.java
+++ src/main/java/com/langthang/config/WebSocketConfig.java
@@ -1,14 +1,30 @@
 package com.langthang.config;
 
+import com.langthang.services.JwtTokenServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private JwtTokenServices tokenServices;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -20,5 +36,29 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/socket-server")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+                    String token = accessor.getFirstNativeHeader("Authorization");
+                    if (token != null) {
+                        token = token.substring(7);
+
+                        if (tokenServices.isValidToken(token)) {
+                            Authentication auth = tokenServices.getAuthentication(token);
+                            accessor.setUser(auth);
+                        }
+                    }
+                }
+
+                return message;
+            }
+        });
     }
 }
