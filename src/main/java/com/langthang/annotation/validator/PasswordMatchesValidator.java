b39ace73e1312c5cc97af82e690da2ab0d90306a
trinhdvt <trinhvideo123@gmail.com>
2021-05-18 21:17:47 +0700

    fix bug that Validator cannot read private field of object

--- src/main/java/com/langthang/annotation/validator/PasswordMatchesValidator.java
+++ src/main/java/com/langthang/annotation/validator/PasswordMatchesValidator.java
@@ -15,14 +15,15 @@ public class PasswordMatchesValidator implements ConstraintValidator<PasswordMat
 
     @Override
     public boolean isValid(Object object, ConstraintValidatorContext context) {
-
         try {
             Class<?> clazz = object.getClass();
-            Field passwordField = clazz.getField("password");
-            Field passMatchesField = clazz.getField("matchedPassword");
+            Field passwordField = clazz.getDeclaredField("password");
+            Field passMatchesField = clazz.getDeclaredField("matchedPassword");
 
-            return passwordField.get(object).equals(passMatchesField.getLong(object));
+            passwordField.setAccessible(true);
+            passMatchesField.setAccessible(true);
 
+            return passwordField.get(object).equals(passMatchesField.get(object));
         } catch (NoSuchFieldException | IllegalAccessException e) {
             return false;
         }
