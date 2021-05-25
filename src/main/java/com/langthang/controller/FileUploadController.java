a146c179399b1c987b8ec6afe1e0e4044064bf32
trinhdvt <trinhvideo123@gmail.com>
2021-05-25 14:27:06 +0700

    refactor code

--- src/main/java/com/langthang/controller/FileUploadController.java
+++ src/main/java/com/langthang/controller/FileUploadController.java
@@ -7,13 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping
 @Validated
 public class FileUploadController {
 
