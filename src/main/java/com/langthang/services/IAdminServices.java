c43c33a1450f81bb8877b50c73f5c1283c548271
trinhdvt <trinhvideo123@gmail.com>
2021-06-03 10:05:16 +0700

    refactor code, rename method

--- src/main/java/com/langthang/services/IAdminServices.java
+++ src/main/java/com/langthang/services/IAdminServices.java
@@ -9,13 +9,11 @@ import java.util.List;
 public interface IAdminServices {
     SystemReportDTO getBasicSystemReport();
 
-    void updateUserToAdmin(int userId);
+    void assignRoleAdminToUser(int userId);
 
-    List<PostReportDTO> getPostReport(Pageable pageable);
+    List<PostReportDTO> getListOfPostReport(Pageable pageable);
 
     PostReportDTO getPostReportById(int reportId);
-
-    void createReport(String reportAccount, int postId, String reportContent);
 
     PostReportDTO solveReport(int reportId, String decision);
 }
