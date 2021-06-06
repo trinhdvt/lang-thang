b9f303f34a814b3daabd8945b78fd967c304d1fa
trinhdvt <trinhvideo123@gmail.com>
2021-06-06 23:32:37 +0700

    change Bean class to TaskExecutor to override existing SynchronousTaskExecutor

--- src/main/java/com/langthang/config/AsynchronousSpringEventsConfig.java
+++ src/main/java/com/langthang/config/AsynchronousSpringEventsConfig.java
@@ -1,23 +1,21 @@
 package com.langthang.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsynchronousSpringEventsConfig {
 
     @Bean("taskExecutor")
-    public Executor taskExecutor() {
+    public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(20);
@@ -29,7 +27,7 @@ public class AsynchronousSpringEventsConfig {
 
     @Bean
     @DependsOn({"taskExecutor"})
-    public ApplicationEventMulticaster applicationEventMulticaster(@Qualifier("taskExecutor") @Autowired Executor executor) {
+    public ApplicationEventMulticaster applicationEventMulticaster(@Qualifier("taskExecutor") TaskExecutor executor) {
         SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
         eventMulticaster.setTaskExecutor(executor);
         return eventMulticaster;
