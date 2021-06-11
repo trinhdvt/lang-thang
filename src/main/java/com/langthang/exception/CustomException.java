b2413c40388ca68dde41926f4599971b89b5d5e2
trinhdvt <trinhvideo123@gmail.com>
2021-06-11 17:42:56 +0700

    remove inheritance attributes from RuntimeException when serialize

--- src/main/java/com/langthang/exception/CustomException.java
+++ src/main/java/com/langthang/exception/CustomException.java
@@ -1,10 +1,13 @@
 package com.langthang.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.http.HttpStatus;
 
-
+@JsonIgnoreProperties({"cause", "stackTrace", "localizedMessage", "suppressed"})
 public class CustomException extends RuntimeException {
+
     private final String message;
+
     private final HttpStatus httpStatus;
 
     public CustomException(String message, HttpStatus httpStatus) {
