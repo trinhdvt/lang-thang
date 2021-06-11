6771731aa286e09df9dc4eb015cc19db6f101fb3
trinhdvt <trinhvideo123@gmail.com>
2021-06-11 17:41:59 +0700

    enable caching

--- src/main/java/com/langthang/LangThangApplication.java
+++ src/main/java/com/langthang/LangThangApplication.java
@@ -2,11 +2,13 @@ package com.langthang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableCaching
 public class LangThangApplication {
 
     public static void main(String[] args) {
@@ -14,7 +16,7 @@ public class LangThangApplication {
     }
 
     @PostConstruct
-    public void setTimeZone(){
+    public void setTimeZone() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
     }
 }
