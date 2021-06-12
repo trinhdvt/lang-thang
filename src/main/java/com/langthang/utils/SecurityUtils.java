7d28306675608b5939843408b990652a8d704a69
trinhdvt <trinhvideo123@gmail.com>
2021-06-12 16:49:05 +0700

    add class to manage method interaction with Security

---
+++ src/main/java/com/langthang/utils/SecurityUtils.java
@@ -0,0 +1,21 @@
+package com.langthang.utils;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtils {
+
+    public static String getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        } else {
+            return authentication.getName();
+        }
+    }
+
+}
