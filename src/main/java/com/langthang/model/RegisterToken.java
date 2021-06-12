7f7d6a969fd5dc8922fc7e2b231b59731667074c
trinhdvt <trinhvideo123@gmail.com>
2021-06-12 22:08:50 +0700

    remove expiration of RegisterToken

--- src/main/java/com/langthang/model/RegisterToken.java
+++ src/main/java/com/langthang/model/RegisterToken.java
@@ -3,15 +3,11 @@ package com.langthang.model;
 import lombok.Getter;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
 
 @Entity
 @Table(name = "register_token")
 @Getter
 public class RegisterToken {
-    private static final int EXPIRATION = 60 * 24 * 365;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,27 +19,12 @@ public class RegisterToken {
     @JoinColumn(nullable = false, name = "account_id")
     private Account account;
 
-    @Column(name = "expire_date")
-    private Date expireDate;
-
     public RegisterToken() {
     }
 
     public RegisterToken(String token, Account account) {
         this.token = token;
         this.account = account;
-        this.expireDate = calculateExpireDate(EXPIRATION);
     }
 
-    public void updateToken(String token) {
-        this.token = token;
-        this.expireDate = calculateExpireDate(EXPIRATION);
-    }
-
-    private Date calculateExpireDate(int expiryTimeInMinutes) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Timestamp(cal.getTime().getTime()));
-        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
-        return new Date(cal.getTime().getTime());
-    }
 }
