22f12681ccd0463c5c66dcc32eff0dc147978aff
trinhdvt <trinhvideo123@gmail.com>
2021-06-12 16:45:21 +0700

    use Caffeine as CacheManager

---
+++ src/main/java/com/langthang/config/CaffeineCacheConfig.java
@@ -0,0 +1,27 @@
+package com.langthang.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CaffeineCacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeineConfig());
+        return cacheManager;
+    }
+
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .initialCapacity(5)
+                .maximumSize(10_000)
+                .expireAfterWrite(10, TimeUnit.MINUTES);
+    }
+}
