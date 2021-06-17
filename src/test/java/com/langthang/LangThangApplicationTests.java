4f1c5537b4a3fea0120bd304efcb47f2396bda14
trinhdvt <trinhvideo123@gmail.com>
2021-06-17 14:12:01 +0700

    remove all test

--- src/test/java/com/langthang/LangThangApplicationTests.java
+++ src/test/java/com/langthang/LangThangApplicationTests.java
@@ -1,23 +1,16 @@
 package com.langthang;
 
+import com.langthang.job.BackupDatabaseJob;
 import com.langthang.services.IStorageServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
 class LangThangApplicationTests {
-
-    @Autowired
-    private IStorageServices storageServices;
-
-    @Test
-    void contextLoads() {
-        List<String> objectsKey = Arrays.asList("71645695_p0.png", "1621696953695_avatar.jpeg");
-        storageServices.deleteImages(objectsKey);
-    }
 
 }
