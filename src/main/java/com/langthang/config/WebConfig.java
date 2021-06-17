acce0eec6ccd17add3d7a60348e4a85f306a0f76
trinhdvt <trinhvideo123@gmail.com>
2021-06-17 14:09:06 +0700

    add /api to all API endpoint

--- src/main/java/com/langthang/config/WebConfig.java
+++ src/main/java/com/langthang/config/WebConfig.java
@@ -2,7 +2,10 @@ package com.langthang.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,6 +17,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**");
+            }
+
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                configurer.addPathPrefix("/api",
+                        HandlerTypePredicate.forAnnotation(RestController.class));
             }
         };
     }
