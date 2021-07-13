6a84cfc6927e8550daf5d53c58cc454003a06e59
trinhdvt <trinhvideo123@gmail.com>
2021-07-13 22:13:47 +0700

    change type from String[] -> List<String>

--- src/main/java/com/langthang/dto/PostRequestDTO.java
+++ src/main/java/com/langthang/dto/PostRequestDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 public class PostRequestDTO {
@@ -18,7 +19,7 @@ public class PostRequestDTO {
     private String content;
 
     @Size(max = 5, message = "Maximum 5 categories")
-    private String[] categories;
+    private List<String> categories;
 
     @NotNull
     @NotBlank(message = "A post should have a thumbnail")
