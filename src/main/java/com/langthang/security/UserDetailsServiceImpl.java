8cbb4b825d1641c083e49c2626288a1ffd639126
trinhdvt <trinhvideo123@gmail.com>
2021-12-29 16:49:35 +0700

    implement custom UserDetails

--- src/main/java/com/langthang/security/UserDetailsServiceImpl.java
+++ src/main/java/com/langthang/security/UserDetailsServiceImpl.java
@@ -2,18 +2,13 @@ package com.langthang.security;
 
 import com.langthang.model.Account;
 import com.langthang.repository.AccountRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-
 @Service
-@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final AccountRepository accRepo;
@@ -31,12 +26,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("Account with " + email + " not found!");
         }
 
-        return new User(acc.getEmail()
-                , acc.getPassword()
-                , acc.isEnabled()
-                , true
-                , true
-                , true,
-                Collections.singleton(acc.getRole()));
+        return new CurrentUser(acc.getEmail(),
+                acc.getPassword(),
+                acc.isEnabled(),
+                acc.getId(),
+                acc.getRole());
     }
 }
