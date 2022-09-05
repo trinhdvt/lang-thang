3fb122864dbeda7be00e4fa1fe4097d186ede2f8
dvt <45824015+trinhdvt@users.noreply.github.com>
2022-09-05 16:34:08 +0700

    Delete frontend dir

--- frontend-travel-blog/src/users/body/category/enum/category-apis.js
+++
@@ -1,7 +0,0 @@
-const baseUrl = '/api/category'
-
-export default {
-    getPostsByCategory(categoryId){
-        return `${baseUrl}/${categoryId}/post`
-    }
-}
--- frontend-travel-blog/src/users/body/notification/enum/notification-apis.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = '/api/notifications'
-
-export default {
-    seenNotification(notificationId){
-        return `${baseUrl}/${notificationId}/seen`
-    },
-    getNotifications: `${baseUrl}`,
-    seenAllNotification: `${baseUrl}/seenAll`
-}
